dd13794032b43dd61ac42b0a8fcebb5a51a85fc6
Carl Harrison <carlharrison1@live.co.uk>
2021-04-11 07:22:18 +0100

    Started chat message data return

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,14 +1,19 @@
 import React from "react";
-import { Text } from "react-native";
-
+import { Text, FlatList } from "react-native";
 import { useRoute } from '@react-navigation/native';
+
+import chatRoomData from '../data/Chats';
+import ChatMessage from "../components/ChatMessage";
 
 const ChatRoomScreen = () => {
 
     const route = useRoute();
 
     return (
-        <Text>Chat Room</Text>
+        <FlatList
+            data={chatRoomData.messages}
+            renderItem={({ item }) => <ChatMessage message={item} />}
+        />
     )
 }
 
