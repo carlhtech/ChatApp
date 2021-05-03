90a3e9c66d52031d3b19c6a29c10312147dfe273
Carl Harrison <carlharrison1@live.co.uk>
2021-05-03 17:04:09 +0100

    Started work on auth in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
@@ -7,8 +7,9 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
 import { withAuthenticator } from "aws-amplify-react-native";
+import { getUser } from './src/graphql/queries';
 
-import Amplify from 'aws-amplify'
+import Amplify, { Auth, API, graphqlOperation } from 'aws-amplify'
 import config from './src/aws-exports'
 Amplify.configure(config)
 
@@ -16,6 +17,27 @@ Amplify.configure(config)
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  // run only when first mounted
+  useEffect(() => {
+    const fetchUser = async () => {
+      // get autheticated user from auth
+      const userInfo = await Auth.currentAuthenticatedUser({ bypassCache: true });
+
+      if (userInfo) {
+        const userData = await API.graphql(
+          graphqlOperation(
+            getUser, { id: userInfo.attributes.sub }
+          )
+        )
+
+        if (userData.data.getUser) {
+          console.log("User is already registered in database")
+        }
+      }
+    }
+    fetchUser();
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
